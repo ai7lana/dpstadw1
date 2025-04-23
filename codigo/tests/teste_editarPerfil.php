7f2d7dcfb1664f77123058aef870ce889c6271e9
isally <isallly277@gmail.com>
2025-04-23 09:19:37 -0300

    editar perfil

--- codigo/tests/teste_editarPerfil.php
+++ codigo/tests/teste_editarPerfil.php
@@ -3,8 +3,13 @@
 require_once "../conexao.php";
 require_once "../funcoes.php";
 
+$idperfil = 1;
+$nome = "nama";
+$nome_perfil = "namasama";
+$senha = "123";
+$email = "namasama@gmail.com";
 
-editarPerfil($conexao, $nome, $nome_perfil, $usuario_idusario,  $email, $senha);
+editarPerfil($conexao, $nome, $nome_perfil,  $email, $senha);
 
 echo "<pre>";
 print_r(listarPerfil($conexao));
