748039abecdb497d8232f5caeccc3e068c67db82
is4lly <isallly277@gmail.com>
2025-06-12 14:13:51 -0300

    Update teste_pesquisarAvaliacaoId.php

--- codigo/tests/teste_pesquisarAvaliacaoId.php
+++ codigo/tests/teste_pesquisarAvaliacaoId.php
@@ -4,8 +4,9 @@ require_once "../conexao.php";
 require_once "../funcoes.php";
 
 $perfil_idperfil = 1;
+$receita_idreceita = 2;
 
 echo "<pre>";
-print_r(pesquisarReceitaNome($conexao, $perfil_idperfil));
+print_r(pesquisarReceitaNome($conexao, $perfil_idperfil, $receita_idreceita));
 echo "</pre>";
 ?>
