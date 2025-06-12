7bad1f171ad9e3a170f03793724bb867347ac786
ai7lana <alana.vieira@estudante.ifgoiano.edu.br>
2025-06-12 10:15:40 -0300

    Create teste_pesquisarReceitaPerfil.php

---
+++ codigo/tests/teste_pesquisarReceitaPerfil.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "../conexao.php";
+require_once "../funcoes.php";
+
+$perfil_idperfil = 1;
+
+echo "<pre>";
+print_r(pesquisarReceitaNome($conexao, $perfil_idperfil));
+echo "</pre>";
+?>
