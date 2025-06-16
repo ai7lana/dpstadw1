f9143cc32ff75d3e94046b9b9085615898864290
is4lly <isallly277@gmail.com>
2025-06-16 13:52:54 -0300

    Update teste_salvarAvaliacao.php

--- codigo/tests/teste_salvarAvaliacao.php
+++ codigo/tests/teste_salvarAvaliacao.php
@@ -5,8 +5,8 @@ require_once "../funcoes.php";
 
 $comentario = "Receita boa!";
 $nota = "5" ;
-$receita_idreceita = 2;
-$perfil_idperfil = 3; 
+$receita_idreceita = 1;
+$perfil_idperfil = 1; 
 
 salvarAvaliacao ($conexao, $comentario, $nota, $receita_idreceita, $perfil_idperfil);
 
