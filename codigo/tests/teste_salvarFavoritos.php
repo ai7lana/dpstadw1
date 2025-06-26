d18b3c0cff8994bfad351d87ea96183681ecdb0e
is4lly <isallly277@gmail.com>
2025-06-26 08:37:37 -0300

    Update teste_salvarFavoritos.php

--- codigo/tests/teste_salvarFavoritos.php
+++ codigo/tests/teste_salvarFavoritos.php
@@ -2,8 +2,8 @@
 require_once "../conexao.php";
 require_once "../funcoes.php";     
 
-$perfil_idperfil = 2;            
-$receita_idreceita = 3; 
+$perfil_idperfil = 1;            
+$receita_idreceita = 1; 
 
 salvarFavoritos($conexao, $perfil_idperfil, $receita_idreceita);
 ?>
