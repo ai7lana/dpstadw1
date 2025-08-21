c7623002afc0cd8d351a42dbf66444501f0e9a65
is4lly <isallly277@gmail.com>
2025-08-21 16:02:05 -0300

    Update index.php

--- codigo/public/index.php
+++ codigo/public/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>fazer login</title>
+    <script src="../jquery-3.7.1.min.js"></script>
 </head>
 <body>
     <h1>Primeira vez aqui?</h1>
