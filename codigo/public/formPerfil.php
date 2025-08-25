8334d1024fe5c4df755bb3771481fb0ea1b3326d
ai7lana <alana.vieira@estudante.ifgoiano.edu.br>
2025-08-25 14:01:28 -0300

    Create formPerfil.php

---
+++ codigo/public/formPerfil.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../jquery-3.7.1.min.js"></script>
+    <title>home</title>
+</head>
+<body>
+    <h2>Cadastro de Perfil</h2>
+    <form>
+        <input type="text" name="nome" id="nome" placeholder= "Nome completo" required><br><br>
+
+        <input type="text" name="nome_perfil" id="nome_perfil" placeholder="Nome do perfil" required><br><br>
+
+        <input type="email" name="email" id="email" placeholder="E-mail" required><br><br>
+
+        <input type="password" name="senha" id="senha" placeholder="Senha" required><br><br>
+
+        <button type="submit">Cadastrar</button>
+    </form>
+</body>
+</html>
