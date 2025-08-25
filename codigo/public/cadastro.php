c08797e44f89ecf9d7063f08c434c4698bbf58ae
is4lly <isallly277@gmail.com>
2025-08-25 14:26:07 -0300

    arrumando o index

---
+++ codigo/public/cadastro.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fazer login</title>
+    <script src="../jquery-3.7.1.min.js"></script>
+</head>
+<body>
+    <h1>Primeira vez aqui?</h1>
+    <h2>Faça seu cadastro!</h2> 
+    <p>Coloque seu nome:</p> <br>
+    <input type="text" name="nome" id="nome"> <br><br>
+    <p>Escolha um número de usuario:</p> <br>
+    <input type="text" name="nomeusuario" id="nomeusu"> <br><br>
+    <p>DIgite seu email:</p> <br>
+    <input type="text" name="email" id="email"> <br><br>
+    <p>Digite sua senha:</p> <br>
+    <input type="password" name="senha" id="senha"> <br><br>
+
+    
+</body>
+</html>
