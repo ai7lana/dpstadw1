b8179c666a31e817fd784b30db196a19cd1406e6
is4lly <isallly277@gmail.com>
2025-08-28 08:20:03 -0300

    Update index.php

--- codigo/public/index.php
+++ codigo/public/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>index</title>
     <script src="../jquery-3.7.1.min.js"></script>
-    <style src="style.css"></style>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1 class = "titulo" id="brasilna">Brasil na Cozinha</h1>
