7122a7d72a4397c703deda12b46b8cd8deb0be9e
ai7lana <alana.vieira@estudante.ifgoiano.edu.br>
2025-08-28 15:44:24 -0300

    pastas

--- codigo/public/formFavoritos.php
+++ codigo/public/formFavoritos.php
@@ -7,6 +7,14 @@
     <script src="../jquery-3.7.1.min.js"></script>
 </head>
 <body>
+    <h2>Cadastro de Receita</h2>
+    <form action="../salvarFavoritos.php" method="post">
+
     
+    Perfil: <br><input type="text" name="perfil" required><br>
+    Receita: <br> <input type="text" name="Receita" required><br>
+
+
+
 </body>
 </html>
--- codigo/salvarPerfil.php
+++ codigo/salvarPerfil.php
@@ -7,12 +7,9 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 $nome_perfil = $_POST['nome_perfil'];
+salvarPerfil($conexao, $nome, $email, $senha, $nome_perfil);
 
-if ($id == 0) {
-    salvarPerfil($conexao, $nome, $email, $senha, $nome_perfil);
-} else {
-    editarPerfil($conexao, $nome, $email, $senha, $nome_perfil, $id_perfil);
-}
+
 
 header("Location: /public/formPerfil.php");
 ?>
