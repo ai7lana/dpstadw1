d5bafcdf3d69b15ca626658d66b5f73585a18065
is4lly <isallly277@gmail.com>
2025-08-28 08:49:58 -0300

    deus

--- codigo/public/index.php
+++ codigo/public/index.php
@@ -9,17 +9,30 @@
 </head>
 <body class="bodyindex">
     <div id="cabeca">
-        <div>
-            <p class="tit" id="titulo">Brasil</p>
-            <p class="tit" id="titulo2"> na Cozinha</p>
+        <div class="titu" id="titulogeral">
+            <p class="tit" id="titulo">Brasil <br> na Cozinha</p>
         </div>
-        <p class="link" id="inicio">início</p>
-        <p class="link" id="perfil">perfil</p>
-        <p class="link" id="receitas">receitas</p>
-        <p class="link" id="buscat">buscar</p>
-        <p class="link" id="sobre">sobre nós </p>
-        <p class="link" id="cadastrar">cadastrar</p>
-        <p class="link" id="login">login</p>
+        <div>
+            <p class="link" id="inicio">início</p>
+        </div>
+        <div>
+            <p class="link" id="perfil">perfil</p>
+        </div>
+        <div>
+            <p class="link" id="receitas">receitas</p>
+        </div>
+        <div>
+            <p class="link" id="buscat">buscar</p>
+        </div>
+        <div>
+            <p class="link" id="sobre">sobre nós </p>
+        </div>
+        <div>
+            <p class="link" id="cadastrar">cadastrar</p>
+        </div>
+        <div>
+            <p class="link" id="login">login</p>
+        </div>
     </div>
 </body>
 </html>
