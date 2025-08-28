47b998a38f98a5af7e1fdfaf899d127e8da83744
is4lly <isallly277@gmail.com>
2025-08-28 16:24:02 -0300

    index

--- codigo/public/index.php
+++ codigo/public/index.php
@@ -8,26 +8,29 @@
     <link rel="stylesheet" href="style/style.css">
 </head>
 <body class="bodyindex">
-    <div id="cabeca"> <br> <br><br> <br><br><br><br><br><br><br>
-        <div class="titu" id="titulogeral"> <br>
-            <a href="" class="tit" id="titulo"> Brasil <br> na Cozinha</a>
-        </div>
-        <div class="divlinks" id="divinicio">
-            <a href="" class="link" id="inicio"> ÍNICIO </a>
-        </div>
-        <div class="divlinks" id="divperfil">
-            <a href="" class="link" id="perfil"> PERFIL</a>
-        </div>
-        <div class="divlinks" id="divreceitas">
-            <a href="" class="link" id="receitas">RECEITAS</a>
-        </div>
-        <div class="divlinks" id="divbuscar">
-            <a href="" class="link" id="buscar">BUSCAR</a>
-        </div>
-        <div class="divlinks" id="divsobre">
-            <a href="" class="link" id="sobre">SOBRE NÓS</a>
-        </div>
+    <div id="fundo"> <br> <br><br> <br><br><br><br><br><br><br>
+        <div id="cabeca">
+            <div class="titu" id="titulogeral"> <br>
+                <a href="" class="tit" id="titulo"> Brasil <br> na Cozinha</a>
+            </div>
+            <div class="divlinks" id="divinicio">
+                <a href="" class="link" id="inicio"> &nbsp&nbsp ÍNICIO &nbsp&nbsp </a>
+            </div>
+            <div class="divlinks" id="divperfil">
+                <a href="" class="link" id="perfil"> &nbsp&nbsp PERFIL &nbsp&nbsp</a>
+            </div>
+            <div class="divlinks" id="divreceitas">
+                <a href="" class="link" id="receitas"> &nbsp&nbsp RECEITAS &nbsp&nbsp </a>
+            </div>
+            <div class="divlinks" id="divbuscar">
+                <a href="" class="link" id="buscar"> &nbsp&nbsp BUSCAR &nbsp&nbsp</a>
+            </div>
+            <div class="divlinks" id="divsobre">
+                <a href="" class="link" id="sobre"> &nbsp&nbsp SOBRE NÓS &nbsp&nbsp</a>
+            </div>
+            </div>
     </div>
+
     <div id="cabeca2"> <br><br><br>
         <div class="divlinks" id="divcadastrar">
             <a href="formPerfil.php" class="link" id="cadastrar"> CADASTRAR &nbsp&nbsp
