ce5b59834ccdacea35192f0a3747a1ef6c866565
ai7lana <alana.vieira@estudante.ifgoiano.edu.br>
2025-09-11 16:22:51 -0300

    tentando

---
+++ codigo/public/paginicial.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Brasil na Cozinha</title>
+  <link rel="stylesheet" href="style/style.css">
+</head>
+<body id="bodyinicial">
+  <div class="logo">
+    <img src="logo.png" alt="Logo Brasil na Cozinha">
+  </div>
+
+  <div class="painel">
+    <div class="boas-vindas">Seja bem-vindo (a)</div>
+    
+    <!-- Botão "Entrar" com link -->
+    <a href="index.php">
+      <button class="botaopag">entrar</button>
+    </a>
+
+    <!-- Botão "Cadastrar" com link -->
+    <a href="formPerfil.php">
+      <button class="botaopag" >cadastrar</button>
+    </a>
+  </div>
+
+  <img src="gatocaixa.png" alt="Gato na caixa" class="gato-caixa">
+</body>
+</html>
