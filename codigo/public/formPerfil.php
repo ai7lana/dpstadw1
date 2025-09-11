9b4fc2d2de96affba1df658cc8d6f4b5305e9765
ai7lana <alana.vieira@estudante.ifgoiano.edu.br>
2025-09-11 10:10:22 -0300

    Update formPerfil.php

--- codigo/public/formPerfil.php
+++ codigo/public/formPerfil.php
@@ -6,18 +6,24 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <script src="../jquery-3.7.1.min.js"></script>
+  <link rel="stylesheet" href="styles.css">
 </head>
-<body>
-  <h2>Criar Perfil</h2>
-  <form action="../salvarPerfil.php" method="POST">
+<body id="bodyperfil">
+  <div class="container">
+    <h2>Criar Perfil</h2>
+      <form class="form-container" action="../salvarPerfil.php" method="POST">
     
-    Nome: <br><input type="text" name="nome" required><br>
-    Nome de Usuário: <br> <input type="text" name="nome_perfil" required><br>
-    Email: <br> <input type="email" name="email" required><br>
-    Senha: <br> <input type="password" name="senha" required><br>
+      Nome: <br><input type="text" name="nome" id="nome" required><br>
+      Nome de Usuário: <br> <input type="text" name="nome_perfil" id="usuario" required><br>
+      Email: <br> <input type="email" name="email" id="email" required><br>
+      Senha: <br> <input type="password" name="senha" id="senha" required><br>
 
-    <button type="submit">Salvar</button>
-</form>
+      <button type="submit">Salvar</button>
+      <p>Já tem uma conta? <a href="#">Entrar</a></p>
+      </form>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="app.js"></script>
 
 </body>
 </html>
