4328a64e43d74d6b402654e7d2bf92adb9404d25
is4lly <isallly277@gmail.com>
2025-09-25 08:28:28 -0300

    reset.css

--- codigo/public/home.php
+++ codigo/public/home.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="../jquery-3.7.1.min.js"></script>
+    <link rel="stylesheet" href="style/reset.css">
+    <link rel="stylesheet" href="style/style.css">
     <title>home</title>
 </head>
 <body>
--- codigo/public/index.php
+++ codigo/public/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>index</title>
     <script src="../jquery-3.7.1.min.js"></script>
+    <link rel="stylesheet" href="style/reset.css">
     <link rel="stylesheet" href="style/style.css">
 </head>
 
