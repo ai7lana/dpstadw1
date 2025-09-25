cdd52b3da6b44be46f1129733b60aaf85c08e882
is4lly <isallly277@gmail.com>
2025-09-25 10:26:10 -0300

    home

--- codigo/public/home.php
+++ codigo/public/home.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,16 +9,18 @@
     <link rel="stylesheet" href="style/style.css">
     <title>home</title>
 </head>
+
 <body class="bodyhome">
     <div id="logohome">
         <img src="style/images/logosemfundo.png" alt="">
     </div>
     <div id="tithome">
-        <p id='titulohome'> Seja Bem-Vindo(a)!</p>
+        <p class='titulohome'> Seja Bem-Vindo(a)!</p>
     </div>
     <div class="butoesdiv">
         <button class="butoes" id="butaocadastrar" type="button"> Cadastrar</button> <br><br>
         <button class="butoes" id="butaologin" type="button">Entrar</button>
     </div>
 </body>
+
 </html>
