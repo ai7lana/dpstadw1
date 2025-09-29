59940d5eff140c618fba604fb53f74be4c626ae4
is4lly <isallly277@gmail.com>
2025-09-29 13:45:03 -0300

    formcadastrarPerful

--- codigo/public/formPerfil.php
+++ codigo/public/formPerfil.php
@@ -1,28 +1,39 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <script src="../jquery-3.7.1.min.js"></script>
-  <link rel="stylesheet" href="style/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faça seu cadastro!</title>
+    <script src="../jquery-3.7.1.min.js"></script>
+    <link rel="stylesheet" href="style/reset.css">
+    <link rel="stylesheet" href="style/style.css">
 </head>
-<body id="bodyperfil">
-  <div class="container">
-      <form class="form-container" action="../salvarPerfil.php" method="POST">
-    
-      Nome: <br><input type="text" name="nome" id="nome" required><br>
-      Nome de Usuário: <br> <input type="text" name="nome_perfil" id="usuario" required><br>
-      Email: <br> <input type="email" name="email" id="email" required><br>
-      Senha: <br> <input type="password" name="senha" id="senha" required><br>
-
-      <button type="submit">Salvar</button>
-      <p>Já tem uma conta? <a href="#">Entrar</a></p>
-      </form>
-  </div>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="app.js"></script>
-
+<body class="bodycl">
+    <div class="caixafundocadastro">
+        <div class="titulocl"> <br><br>
+            <p>Primeira vez aqui?</p>
+            <p>Faça seu cadastro!</p>  <br>
+        </div>
+        <div class="clpreencher">
+            <div class="inputgroup">
+                <label>Nome de usuário:</label> <br>
+                <input class="inputcl" type="text" name="nome" id="nome"  placeholder="Digite seu nome"> <br><br>
+            </div>
+            <div class="inputgroup">
+                <label>Nome:</label> <br>
+                <input class="inputcl" type="text" name="nomeusuario" id="nomeusu" placeholder="Digite o nome de usuário"> <br><br>
+            </div>
+            <div class="inputgroup">
+                <label>E-mail:</label> <br>
+                <input class="inputcl" type="text" name="email" id="email" placeholder="Digite seu E-mail"> <br><br>
+            </div>
+            <div class="inputgroup">
+                <label>Senha:</label> <br>
+                <input class="inputcl" type="password" name="senha" id="senha" placeholder="Digite sua senha"> <br><br>
+            </div>
+        </div>
+        <a href="" class="botaosubimit"><button type="button" class="botaosub" id="botaocadastro">cadastrar</button></a> <br>
+        <a href="login.html" class="linklogin">Já possui uma conta? Clique aqui!</a>
+    </div>
 </body>
 </html>
