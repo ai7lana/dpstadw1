1cf9e65db29b3fec1eb15e48dfc9c6060eae90ff
is4lly <isallly277@gmail.com>
2025-09-29 13:47:36 -0300

    asdasd

---
+++ codigo/public/formLoginPerfil.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faça seu Login!</title>
+    <script src="../jquery-3.7.1.min.js"></script>
+    <link rel="stylesheet" href="style/reset.css">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body class="bodycl">
+    <div class="caixafundologin">
+        <div class="titulocl"> <br><br>
+            <p>Olá de novo!</p>
+            <p>Faça seu Login!</p>  <br>
+        </div>
+        <div class="clpreencher">
+            <div class="inputgroup">
+                <label>Nome de Úsuario:</label> <br>
+                <input class="inputcl" type="text" name="nomeusuario" id="nomeusu" placeholder="Digite o nome de usuário"> <br><br>
+            </div>
+            <div class="inputgroup">
+                <label>Senha:</label> <br>
+                <input class="inputcl" type="password" name="senha" id="senha" placeholder="Digite sua senha"> <br><br>
+            </div>
+        </div>
+        <a href="" class="botaosubimit"><button type="button" class="botaosub" id="botaosubid">entrar</button></a> <br>
+        <a href="login.html" class="linksenha">Esqueceu a senha?</a>
+        <a href="cadastro.html" class="novaconta">Crie uma nova conta</a>
+    </div>
+</body>
+</html>
--- codigo/public/index.php
+++ codigo/public/index.php
@@ -35,11 +35,11 @@
     </div>
     <div id="cabeca2"> <br><br><br>
         <div class="divlinks" id="divcadastrar">
-            <a href="formPerfil.php" class="link" id="cadastrar"> CADASTRAR &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+            <a href="formCadastrarPerfil.php" class="link" id="cadastrar"> CADASTRAR &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
             </a>
         </div>
         <div class="divlinks" id="divlogin">
-            <a href="formPerfil.php" class="link" id="login"> LOGIN &nbsp&nbsp&nbsp</a>
+            <a href="formLoginPerfil.php" class="link" id="login"> LOGIN &nbsp&nbsp&nbsp</a>
         </div>
     </div>
     <div id=imagemindexdiv>
