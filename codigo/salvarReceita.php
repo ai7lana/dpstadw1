d28cf3d6400e7cc0af58b04a9b83e4d3d01d2b7b
ai7lana <alana.vieira@estudante.ifgoiano.edu.br>
2025-10-09 09:53:14 -0300

    Update salvarReceita.php

--- codigo/salvarReceita.php
+++ codigo/salvarReceita.php
@@ -1,6 +1,10 @@
 <?php
+ob_start(); // evita erro de header já enviado
+
 require_once "conexao.php";
 require_once "funcoes.php";
+
+$id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
 
 $nome_comida = $_POST['nome_comida'];
 $tipo = $_POST['tipo'];
@@ -21,14 +25,18 @@ $extensao = pathinfo($nome_arquivo, PATHINFO_EXTENSION);
 $novo_nome = uniqid() . "." . $extensao;
 
 //criar um novo caminho para o arquivo
-// lembre-se de criar a pasta e ajustar as permissões
 $caminho_destino = "./public/foto/" . $novo_nome;
 
 // move a foto para o servidor
 move_uploaded_file($caminho_temporario, $caminho_destino);
 
-salvarReceita($conexao, $nome_comida, $tipo, $ingredientes, $modo_de_preparo, $tempo, $rendimento, $novo_nome, $regiao, $perfil_idperfil);
 
-// header("Location: /public/formReceita.php");
+if ($id == 0) {
+    salvarReceita($conexao, $nome_comida, $tipo, $ingredientes, $modo_de_preparo, $tempo, $rendimento, $novo_nome, $regiao, $perfil_idperfil);
+} else {
+    editarReceita($conexao, $nome_comida, $tipo, $ingredientes, $modo_de_preparo, $tempo, $rendimento, $novo_nome, $regiao, $perfil_idperfil, $id);
+}
+
+header("Location: /public/listarReceita.php");
+exit;
 ?>
-
