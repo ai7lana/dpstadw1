ef516d638f523ae75b7277b4341e8b409185fcd7
ai7lana <alana.vieira@estudante.ifgoiano.edu.br>
2025-10-09 15:56:09 -0300

    Update formPesquisar.php

--- codigo/public/formPesquisar.php
+++ codigo/public/formPesquisar.php
@@ -1,12 +1,27 @@
+<?php
+
+require_once "../funcoes.php";
+require_once "../conexao.php"; 
+
+$sql_code = ' function = pesquisaReceitaID ';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>pesquisar</title>
     <script src="../jquery-3.7.1.min.js"></script>
+
 </head>
 <body>
+    <h1>busque</h1>
+    <form action="" >
+        <input name="buscar" type="text" >
+        <button type="submit">buscar</button>
+    </form>
 
     
 </body>
