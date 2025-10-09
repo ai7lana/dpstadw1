da01bdcdee73e2d5fafad291e0fa828c9c491534
is4lly <isallly277@gmail.com>
2025-10-09 09:53:26 -0300

    catalogar as funções

--- codigo/funcoes.php
+++ codigo/funcoes.php
@@ -1,5 +1,19 @@
 <?php
 
+
+/**
+ * salvar dados do usúario no perfil
+ * 
+ * A par.....
+ * 
+ * @param mysqli $conexao Uma conexão com o banco.
+ * @param string $email O e-mail informado pelo usuário.
+ * @param string $senha A senha informada pelo usuário.
+ * @param string $nome o nome informado pelo usuario.
+ * @param string $nome_perfil o nome de perfol informardo pelo usuario
+ * @return bool perfil salvo
+ * 
+ **/
 function salvarPerfil($conexao, $nome, $nome_perfil, $email, $senha) {
     $sql = "INSERT INTO perfil (nome, nome_perfil, email, senha) VALUES (?, ?, ?, ?)";
     $comando = mysqli_prepare($conexao, $sql);
@@ -11,6 +25,22 @@ function salvarPerfil($conexao, $nome, $nome_perfil, $email, $senha) {
 
 }
 
+/**
+ * salvar dados do da receita no perfil
+ * 
+ * A par.....
+ * 
+ * @param mysqli $conexao Uma conexão com o banco.
+ * @param string $nome_comida o nome da comida informada pelo usuário.
+ * @param string $tipo o tipo de comida informada pelo usuário.
+ * @param string $ingredientes os ingredientes informados pelo usuario.
+ * @param string $modo_de_preparo passo a passo da receita fornecido pelo usuario
+ * @param string $tempo o tempo de preparo fornecido pelo usuario
+ * @param string $rendimento o número de porções fornecido pelo usuario 
+ * @param string $foto foto da receita fornecida pelo usuario
+ * @return bool receita salva
+ * 
+ **/
 function salvarReceita($conexao, $nome_comida, $tipo, $ingredientes, $modo_de_preparo, $tempo, $rendimento, $foto, $regiao, $perfil_idperfil) {
     $sql = "INSERT INTO receita (nome_comida, tipo, ingredientes, modo_de_preparo, tempo, rendimento, foto, regiao, perfil_idperfil) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $comando = mysqli_prepare($conexao, $sql);
@@ -23,6 +53,16 @@ function salvarReceita($conexao, $nome_comida, $tipo, $ingredientes, $modo_de_pr
     return $funcionou;
 }
 
+/**
+ * Salva uma avaliação de receita no banco de dados.
+ *
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param string $comentario O comentário feito pelo usuário.
+ * @param int $nota A nota atribuída pelo usuário.
+ * @param int $receita_idreceita O ID da receita que está sendo avaliada.
+ * @param int $perfil_idperfil O ID do perfil do usuário que fez a avaliação.
+ * @return bool Retorna true se a avaliação foi salva com sucesso, ou false em caso de erro.
+ **/
 function salvarAvaliacao ($conexao, $comentario, $nota, $receita_idreceita, $perfil_idperfil){
     $sql = "INSERT INTO avaliacao (comentario, nota, receita_idreceita, perfil_idperfil)
     VALUES (?, ?, ?, ?)";
@@ -36,6 +76,15 @@ function salvarAvaliacao ($conexao, $comentario, $nota, $receita_idreceita, $per
 
 }
 
+/**
+ * Salva um favorito para um perfil e receita.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param int $perfil_idperfil O ID do perfil.
+ * @param int $receita_idreceita O ID da receita.
+ * @return bool Retorna true se a operação for bem-sucedida, caso contrário, retorna false.
+ **/
+
 function salvarFavoritos($conexao, $perfil_idperfil, $receita_idreceita) {
     $sql = "INSERT INTO favoritos (perfil_idperfil, receita_idreceita) VALUES (?, ?)";
     $comando = mysqli_prepare($conexao, $sql);
@@ -47,7 +96,21 @@ function salvarFavoritos($conexao, $perfil_idperfil, $receita_idreceita) {
     return $funcionou;
 
 }
-
+/**
+ * Edita uma receita no banco de dados.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param string $nome_comida Nome da comida.
+ * @param string $tipo Tipo da receita.
+ * @param string $ingredientes Ingredientes da receita.
+ * @param string $modo_de_preparo Modo de preparo da receita.
+ * @param string $tempo Tempo de preparo.
+ * @param string $rendimento Rendimento da receita.
+ * @param string $foto URL da foto da receita.
+ * @param string $regiao Região da receita.
+ * @param int $perfil_idperfil ID do perfil associado à receita.
+ * @param int $id ID da receita a ser editada.
+ **/
 function editarReceita ($conexao, $nome_comida, $tipo, $ingredientes, $modo_de_preparo, $tempo, $rendimento, 
     $foto, $regiao, $perfil_idperfil, $id){
     $sql = "UPDATE receita SET nome_comida=?, tipo=?, ingredientes=?, modo_de_preparo=?, tempo=?, rendimento=?, foto=?, regiao=?, perfil_idperfil=? WHERE idreceita = ? ";
@@ -59,6 +122,16 @@ function editarReceita ($conexao, $nome_comida, $tipo, $ingredientes, $modo_de_p
     mysqli_stmt_close($comando);
 }
 
+/**
+ * Edita o perfil do usuário no banco de dados.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param string $nome Nome do usuário.
+ * @param string $nome_perfil Nome do perfil do usuário.
+ * @param string $email E-mail do usuário.
+ * @param string $senha A senha do usuário (será criptografada).
+ * @param int $idperfil ID do perfil a ser editado.
+ **/
 function editarPerfil ($conexao, $nome, $nome_perfil, $email, $senha, $idperfil){
     $sql = "UPDATE perfil SET nome = ?, nome_perfil = ?, email = ?, senha = ? WHERE idperfil = ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -72,6 +145,15 @@ function editarPerfil ($conexao, $nome, $nome_perfil, $email, $senha, $idperfil)
 
 }
 
+/**
+ * Edita a avaliação de uma receita feita por um perfil.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param string $comentario O comentário da avaliação.
+ * @param int $nota A nota atribuída (de 1 a 5).
+ * @param int $perfil_idperfil O ID do perfil que fez a avaliação.
+ * @param int $receita_idreceita O ID da receita avaliada.
+ **/
 function editarAvaliacao($conexao, $comentario, $nota, $perfil_idperfil, $receita_idreceita) {
     $sql = "UPDATE avaliacao SET comentario = ?, nota = ? WHERE perfil_idperfil = ? AND receita_idreceita = ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -81,6 +163,13 @@ function editarAvaliacao($conexao, $comentario, $nota, $perfil_idperfil, $receit
     mysqli_stmt_close($comando);
 }
 
+/**
+ * Edita um favorito (alterando perfil e receita).
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param int $perfil_idperfil ID do perfil.
+ * @param int $receita_idreceita ID da receita. 
+ **/
 function editarFavoritos($conexao, $perfil_idperfil, $receita_idreceita){
     $sql = "UPDATE favoritos SET perfil_idperfil = ?, receita_idreceita = ? WHERE perfil_idperfil = ? AND receita_idreceita = ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -90,6 +179,13 @@ function editarFavoritos($conexao, $perfil_idperfil, $receita_idreceita){
     mysqli_stmt_close($comando);
 }
 
+/**
+ * Deleta uma receita do banco de dados.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param int $idreceita O ID da receita a ser deletada.
+ * @return bool Retorna true se a receita for deletada com sucesso, ou false caso contrário.
+ **/
 function deletarReceita ($conexao, $idreceita){
     $sql = "DELETE FROM receita WHERE idreceita = ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -101,6 +197,13 @@ function deletarReceita ($conexao, $idreceita){
     
 }
 
+/**
+ * Deleta o perfil do usuário do banco de dados.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param int $idperfil O ID do perfil a ser deletado.
+ * @return bool Retorna true se o perfil for deletado com sucesso, ou false caso contrário.
+ **/
 function deletarPerfil ($conexao, $idperfil){
     $sql = "DELETE FROM perfil WHERE idperfil = ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -109,9 +212,16 @@ function deletarPerfil ($conexao, $idperfil){
     $funcionou = mysqli_stmt_execute($comando);
     mysqli_stmt_close($comando);
     return $funcionou;
-
 }
 
+/**
+ * Deleta um favorito de um perfil e receita.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param int $perfil_idperfil O ID do perfil.
+ * @param int $receita_idreceita O ID da receita.
+ * @return bool Retorna true se o favorito for deletado com sucesso, ou false caso contrário.
+ **/
 function deletarFavoritos($conexao, $perfil_idperfil, $receita_idreceita) {
     $sql = "DELETE FROM favoritos WHERE perfil_idperfil = ? AND receita_idreceita = ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -122,6 +232,15 @@ function deletarFavoritos($conexao, $perfil_idperfil, $receita_idreceita) {
     return $funcionou;
 }
 
+
+/**
+ * Deleta uma avaliação de um perfil sobre uma receita.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param int $perfil_idperfil O ID do perfil.
+ * @param int $receita_idreceita O ID da receita.
+ * @return bool Retorna true se a avaliação for deletada com sucesso, ou false caso contrário.
+ **/
 function deletarAvaliacao($conexao, $perfil_idperfil, $receita_idreceita) {
     $sql = "DELETE FROM avaliacao WHERE perfil_idperfil = ? AND receita_idreceita = ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -132,6 +251,12 @@ function deletarAvaliacao($conexao, $perfil_idperfil, $receita_idreceita) {
     return $funcionou;
 }
 
+/**
+ * Lista todas as receitas no banco de dados.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @return array Retorna um array de todas as receitas.
+ **/
 function listarReceitas($conexao) {
     $sql = "SELECT * FROM receita";
     $comando = mysqli_prepare($conexao, $sql);
@@ -148,6 +273,12 @@ function listarReceitas($conexao) {
     return $lista_receitas;
 }
 
+/**
+ * Lista todos os perfis cadastrados no banco de dados.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @return array Retorna um array contendo todos os perfis encontrados na tabela "perfil".
+ **/
 function listarPerfil ($conexao){
     $sql = "SELECT * FROM perfil";
     $comando = mysqli_prepare($conexao, $sql);
@@ -165,6 +296,12 @@ function listarPerfil ($conexao){
 
 }
 
+/**
+ * Lista todos os favoritos cadastrados no banco de dados.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @return array Retorna um array contendo todos os favoritos encontrados na tabela "favoritos".
+ **/
 function listarFavorito ($conexao){
     $sql = "SELECT * FROM favoritos";
     $comando = mysqli_prepare($conexao, $sql);
@@ -181,7 +318,12 @@ function listarFavorito ($conexao){
     return $lista_favorito;
 
 }
-
+/**
+ * Lista todas as avaliações cadastradas no banco de dados.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @return array Retorna um array contendo todas as avaliações encontradas na tabela "avaliacao".
+ **/
 function listarAvaliacao ($conexao){
     $sql = "SELECT * FROM avaliacao";
     $comando = mysqli_prepare($conexao, $sql);
@@ -197,6 +339,13 @@ function listarAvaliacao ($conexao){
     return $lista_avaliacao;
 }
 
+/**
+ * Pesquisa uma receita pelo seu ID.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param int $idreceita O ID da receita a ser pesquisada.
+ * @return array|false Retorna um array com os dados da receita, ou false caso não encontre a receita.
+ **/
 function pesquisarReceitaId($conexao, $idreceita) {
     $sql = "SELECT * FROM receita WHERE idreceita = ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -212,6 +361,13 @@ function pesquisarReceitaId($conexao, $idreceita) {
     return $receita;
 }
 
+/**
+ * Pesquisa um perfil pelo seu ID.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param int $idperfil O ID do perfil a ser pesquisado.
+ * @return array|false Retorna um array com os dados do perfil, ou false caso não encontre o perfil.
+ **/
 function pesquisarPerfilId($conexao, $idperfil) {
     $sql = "SELECT * FROM perfil WHERE idperfil = ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -226,6 +382,14 @@ function pesquisarPerfilId($conexao, $idperfil) {
     return $perfil;
 }
 
+/**
+ * Pesquisa uma avaliação pelo ID do perfil e ID da receita.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param int $perfil_idperfil O ID do perfil (usuário).
+ * @param int $receita_idreceita O ID da receita.
+ * @return array|false Retorna um array com os dados da avaliação, ou false caso não encontre a avaliação.
+ **/
 function pesquisarAvaliacaoId($conexao, $perfil_idperfil, $receita_idreceita){
     $sql = "SELECT * FROM avaliacao WHERE perfil_idperfil = ? and receita_idreceita = ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -242,6 +406,13 @@ function pesquisarAvaliacaoId($conexao, $perfil_idperfil, $receita_idreceita){
 }
 
 
+/**
+ * Pesquisa uma receita pelo nome.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param string $nome_comida O nome da comida/receita a ser pesquisada.
+ * @return array|false Retorna um array com os dados da receita, ou false caso não encontre a receita.
+ **/
 function pesquisarReceitaNome($conexao, $nome_comida) {
     $sql = "SELECT * FROM receita WHERE nome_comida LIKE ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -258,6 +429,13 @@ function pesquisarReceitaNome($conexao, $nome_comida) {
     
 }
 
+/**
+ * Pesquisa um perfil pelo nome.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param string $nome O nome do perfil a ser pesquisado.
+ * @return array|false Retorna um array com os dados do perfil, ou false caso não encontre o perfil.
+ **/
 function pesquisarPerfilNome($conexao, $nome) {
     $sql = "SELECT * FROM perfil WHERE nome LIKE ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -273,6 +451,14 @@ function pesquisarPerfilNome($conexao, $nome) {
 
 }
 
+/**
+ * Pesquisa se um perfil tem uma receita em seus favoritos.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param int $perfil_idperfil O ID do perfil.
+ * @param int $receita_idreceita O ID da receita.
+ * @return array|false Retorna um array com os dados dos favoritos, ou false caso não encontre.
+ **/
 function pesquisarFavoritosId($conexao, $perfil_idperfil, $receita_idreceita) {
     $sql = "SELECT * FROM favoritos WHERE perfil_idperfil = ? AND receita_idreceita = ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -289,6 +475,13 @@ function pesquisarFavoritosId($conexao, $perfil_idperfil, $receita_idreceita) {
 
 }
 
+/**
+ * Pesquisa todas as receitas associadas a um perfil.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param int $perfil_idperfil O ID do perfil.
+ * @return array|false Retorna um array com as receitas associadas ao perfil, ou false caso não encontre nenhuma.
+ **/
 function pesquisarReceitaPerfil($conexao, $perfil_idperfil){
     $sql = "SELECT * FROM receita WHERE perfil_idperfil = ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -305,6 +498,13 @@ function pesquisarReceitaPerfil($conexao, $perfil_idperfil){
 
 }
 
+/**
+ * Pesquisa receitas por região.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param string $regiao O nome da região a ser pesquisada.
+ * @return array|false Retorna um array com os dados da receita, ou false caso não encontre nenhuma receita.
+ **/
 function pesquisarReceitaRegiao($conexao, $regiao){
     $sql = "SELECT * FROM receita WHERE regiao LIKE ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -320,6 +520,13 @@ function pesquisarReceitaRegiao($conexao, $regiao){
     return $receita;
 }
 
+/**
+ * Pesquisa receitas por tipo.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param string $tipo O tipo de receita a ser pesquisado.
+ * @return array|false Retorna um array com os dados da receita, ou false caso não encontre nenhuma receita.
+ **/
 function pesquisarReceitaTipo($conexao, $tipo){
     $sql = "SELECT * FROM receita WHERE tipo LIKE ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -335,6 +542,13 @@ function pesquisarReceitaTipo($conexao, $tipo){
     return $receita;
 }
 
+/**
+ * Pesquisa receitas por comentário.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param string $comentario O comentário a ser pesquisado.
+ * @return array|false Retorna um array com os dados da receita, ou false caso não encontre nenhuma receita.
+ **/
 function pesquisarReceitaComentario($conexao, $comentario){
     $sql = "SELECT * FROM comentario LIKE ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -350,6 +564,13 @@ function pesquisarReceitaComentario($conexao, $comentario){
     return $receita;
 }
 
+/**
+ * Pesquisa receitas pelos ingredientes.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param string $ingredientes O nome dos ingredientes a serem pesquisados.
+ * @return array|false Retorna um array com os dados da receita, ou false caso não encontre nenhuma receita.
+ **/
 function pesquiarReceitaIngredientes($conexao, $ingredientes){
     $sql = "SELECT * FROM receita WHERE ingredientes LIKE ?";
     $comando = mysqli_prepare($conexao, $sql);
@@ -365,6 +586,13 @@ function pesquiarReceitaIngredientes($conexao, $ingredientes){
     return $receita;
 }
 
+/**
+ * Pesquisa um perfil pelo nome do perfil.
+ * 
+ * @param mysqli $conexao A conexão com o banco de dados.
+ * @param string $nome_perfil O nome do perfil a ser pesquisado.
+ * @return array|false Retorna um array com os dados do perfil, ou false caso não encontre o perfil.
+ **/
 function pesquisarPerfilNomePerfil($conexao, $nome_perfil){
     $sql = "SELECT * FROM perfil WHERE nome_perfil LIKE ?";
     $comando = mysqli_prepare($conexao, $sql);
