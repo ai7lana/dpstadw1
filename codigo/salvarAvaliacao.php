0b3dc22723bfa52180baded940ab5bf0ada927bb
ai7lana <alana.vieira@estudante.ifgoiano.edu.br>
2025-10-13 13:54:57 -0300

    mds so vamos

--- codigo/salvarAvaliacao.php
+++ codigo/salvarAvaliacao.php
@@ -8,7 +8,11 @@ $receita_idreceita = $_POST['receita_idreceita'];
 $comentario = $_POST ['comentario'];
 $nota = $_POST['nota'];
 
-salvarAvaliacao($conexao, $perfil_idperfil, $receita_idreceita, $comentario, $nota);
+if ($id == 0) {
+    salvarAvaliacao($conexao, $comentario, $nota, $receita_idreceita, $perfil_idperfil);
+} else {
+    editarAvaliacao($conexao, $comentario, $nota, $perfil_idperfil, $receita_idreceita);
+}
 
-header("Location: /public/formFavoritos.php");
+header("Location: /public/formAvaliacao.php");
 ?>
--- codigo/salvarPerfil.php
+++ codigo/salvarPerfil.php
@@ -8,7 +8,12 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 $nome_perfil = $_POST['nome_perfil'];
 
-salvarPerfil($conexao, $nome, $email, $senha, $nome_perfil);
+if ($id == 0) {
+    salvarPerfil($conexao, $nome, $nome_perfil, $email, $senha);
+} else {
+    editarPerfil($conexao, $nome, $nome_perfil, $email, $senha, $idperfil);
+}
+
 
 header("Location: /public/formPerfil.php");
 ?>
