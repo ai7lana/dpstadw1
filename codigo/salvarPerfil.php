d75393e346fee2c376a73aefe5f306209fb924fc
lmao <isallly277@gmail.com>
2025-10-16 08:49:11 -0300

    senha_hash

--- codigo/salvarPerfil.php
+++ codigo/salvarPerfil.php
@@ -10,11 +10,13 @@ $nome_perfil = $_POST['nome_perfil'];
 
 $id = isset($_POST['id']) ? $_POST['id'] : 0;
 
+$senha_hash = password_hash($senha, PASSWORD_DEFAULT);
 
 if ($id == 0) {
-    salvarPerfil($conexao, $nome, $nome_perfil, $email, $senha);
+    salvarPerfil($conexao, $nome, $nome_perfil, $email, $senha_hash);
+    $sql = "INSERT INTO tb_usuario (nome, email, senha, tipo) VALUES ('$nome', '$email', '$senha_hash', 'c')";
 } else {
-    editarPerfil($conexao, $nome, $nome_perfil, $email, $senha, $idperfil);
+    editarPerfil($conexao, $nome, $nome_perfil, $email, $senha_hash, $idperfil);
 }
 
 header("Location: /public/perfil.php");
