9bb5fd5e037bc61ec54b286b54543abe259ad4eb
lmao <isallly277@gmail.com>
2025-10-16 08:42:33 -0300

    cadastrar perfil

--- codigo/salvarPerfil.php
+++ codigo/salvarPerfil.php
@@ -8,12 +8,14 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 $nome_perfil = $_POST['nome_perfil'];
 
+$id = isset($_POST['id']) ? $_POST['id'] : 0;
+
+
 if ($id == 0) {
     salvarPerfil($conexao, $nome, $nome_perfil, $email, $senha);
 } else {
     editarPerfil($conexao, $nome, $nome_perfil, $email, $senha, $idperfil);
 }
 
-
-header("Location: /public/formPerfil.php");
+header("Location: /public/perfil.php");
 ?>
