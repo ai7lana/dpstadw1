c1779df2ed44d6f47790fb5dcfcb5d9b5aaa8aba
is4lly <isallly277@gmail.com>
2025-11-10 16:24:59 -0300

    sfdf

--- codigo/public/editarPerfil.php
+++ codigo/public/editarPerfil.php
@@ -28,7 +28,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Faça seu cadastro!</title>
+    <title>Edite Seu Perfil!</title>
     <script src="../jquery-3.7.1.min.js"></script>
     <link rel="stylesheet" href="style/reset.css">
     <link rel="stylesheet" href="style/style.css">
--- codigo/public/todoPerfil.php
+++ codigo/public/todoPerfil.php
@@ -22,27 +22,32 @@ $nome_perfil_logado = $dadosPerfil['nome_perfil'] ?? 'Usuário desconhecido';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <script src="jquery-3.7.1.min.js"></script>
+    <script src="jquery.validate.min.js"></script>
+    <link rel="stylesheet" href="style/reset.css">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Perfil</title>
 </head>
-<body>
-
-    <h2>Bem-vindo, <?php echo htmlspecialchars($nome_perfil_logado); ?>!</h2>
-    <hr>
+<body class="bodyperfil">
+    <div class="barrasuperior"></div>
+    <div class="barralateral"></div>
     
-    <a href="formReceita.php"> add receita</a> <br>
-    <a href="listarReceita.php"> ver receitas</a> <br>
-    <a href="formPesquisar.php"> buscar receita</a> <br>
-    <a href="listarComentario.php"> avaliacoes </a> <br>
-    <a href="listarFavoritos.php"> Favoritos </a> <br>
-    <a href="receitaPropria.php">  Minhas Receitas </a> <br>
-    <a href="editarPerfil.php">editar perfil</a> <br> <br><br><br>
+    <p class="tituloperfil" id="titperfil"><img src="" alt="">Bem-vindo, <?php echo htmlspecialchars($nome_perfil_logado); ?>!</p>
 
-    <a href="../deslogar.php"> sair </a>
-    
+    <div class="linkperfildiv">
+        <a href="formReceita.php" class="linkperfil" id="receitasp"> <img src="style/images/adicionar.png" alt="" class="addreceita"> adicionar receita</a> <br>
+        <a href="listarReceita.php" class="linkperfil" id="listreceitas"> class="reeitasver">ver receitas</a> <br>
+        <a href="formPesquisar.php" class="linkperfil" id="pesquisarp"> <img src="style/images/pesquisar.png" alt=""> buscar receita</a> <br>
+        <a href="listarComentario.php" class="linkperfil" id="listcomentarios"> <img src="style/images/comentarios.png" alt="">avaliacoes </a> <br>
+        <a href="listarFavoritos.php" class="linkperfil" id="listfav"> <img src="style/images/favoritos.png" alt="" class="favoritos">Favoritos </a> <br>
+        <a href="receitaPropria.php" class="linkperfil" id="minhasreceitas"> Minhas Receitas </a> <br>
+        <a href="editarPerfil.php" class="linkperfil" id="editarp"> <img src="style/images/editarperfil.png" alt="" class="editarperfil">editar perfil</a> <br> <br><br><br>
+    </div>
 
-    <br>
-    <input type="button" value="Voltar" onclick="javascript:history.go(-1)">
-
-     
+    <div class="acoesperfil">
+        <a href="../deslogar.php" class="linkperfil" class="deslogar"> sair </a>
+        <br>
+        <input type="button" value="Voltar" onclick="javascript:history.go(-1)">
+    </div>
 </body>
 </html>
