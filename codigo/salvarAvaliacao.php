0373bf514bbe01a033ad92a2c5234968c1dbfc1a
ai7lana <alana.vieira@estudante.ifgoiano.edu.br>
2025-11-11 14:34:59 -0300

    Update salvarAvaliacao.php

--- codigo/salvarAvaliacao.php
+++ codigo/salvarAvaliacao.php
@@ -1,18 +1,20 @@
 <?php
-
 require_once "conexao.php";
 require_once "funcoes.php";
 
+// Recebe dados do formulário
 $perfil_idperfil = $_POST['perfil_idperfil'];
 $receita_idreceita = $_POST['receita_idreceita'];
-$comentario = $_POST ['comentario'];
+$comentario = $_POST['comentario'];
 $nota = $_POST['nota'];
+$id = isset($_POST['id']) ? $_POST['id'] : 0; // inicializa $id
 
 if ($id == 0) {
     salvarAvaliacao($conexao, $comentario, $nota, $receita_idreceita, $perfil_idperfil);
 } else {
-    editarAvaliacao($conexao, $comentario, $nota, $perfil_idperfil, $receita_idreceita);
+    editarAvaliacao($conexao, $comentario, $nota, $perfil_idperfil, $receita_idreceita, $id);
 }
 
 header("Location: /public/formAvaliacao.php");
+exit;
 ?>
