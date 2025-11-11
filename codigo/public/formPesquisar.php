756e6f5037fe2c66567607b33ddfc9c9f0a8f855
is4lly <isallly277@gmail.com>
2025-11-11 12:40:53 -0300

    adsda

--- codigo/public/formPesquisar.php
+++ codigo/public/formPesquisar.php
@@ -4,17 +4,25 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Pesquise pelas receitas</title>
+  <script src="jquery-3.7.1.min.js"></script>
+  <script src="jquery.validate.min.js"></script>
+  <link rel="stylesheet" href="style/reset.css">
+  <link rel="stylesheet" href="style/style.css">
   <script src="../jquery-3.7.1.min.js"></script>
 
 </head>
 
-<body>
+<body class="bodypesquisarreceitas">
+
+<div class="titulopesquisardiv">
+  <p class="titulopesquisar">Brasil <br> Na Cozinha</p>
+</div>
   <form action="formPesquisar.php">
     Receita: <br>
     <input type="text" name="valor"> <br><br>
-
-    <input type="submit" value="Pesquisar">
+    
+    <input type="submit" value="Pesquisar" class="pesquisarreceitasbotao">
   </form>
   <?php
   if (isset($_GET["valor"]) && !empty($_GET["valor"])) {
@@ -22,7 +30,7 @@
 
     require_once "../conexao.php";
     require_once "../funcoes.php";
-    
+
 
     $receitas = pesquisarReceitaNome($conexao, $valor);
 
@@ -35,14 +43,14 @@
       echo "<th>Receita</th>";
       echo "<th>Foto</th>";
       echo "</tr>";
-      
-      foreach ($receitas as $receita) {
-      echo "<tr>";
 
-      echo "<td>" . $receita ["nome_perfil"]. "</td>";
-      echo "<td>" . $receita["nome_comida"] . "</td>";
-      echo "<td><img src='foto/" . $receita["foto"] . "' alt='Foto da comida' width='100'></td>";
-      echo "</tr>";
+      foreach ($receitas as $receita) {
+        echo "<tr>";
+
+        echo "<td>" . $receita["nome_perfil"] . "</td>";
+        echo "<td>" . $receita["nome_comida"] . "</td>";
+        echo "<td><img src='foto/" . $receita["foto"] . "' alt='Foto da comida' width='100'></td>";
+        echo "</tr>";
       }
     }
   }
