5b2b4f8c5d9b10473c38ae27b618124069821d04
ai7lana <alana.vieira@estudante.ifgoiano.edu.br>
2025-11-13 07:31:42 -0300

    Update listarFavoritos.php

--- codigo/public/listarFavoritos.php
+++ codigo/public/listarFavoritos.php
@@ -1,75 +1,67 @@
 <?php
+session_start();
 require_once "../verificarLogado.php";
 require_once "../conexao.php";
-require_once "../funcoes.php";
 
+// Verifica se o usuário está logado
+if (!isset($_SESSION['id_perfil'])) {
+    die("Erro: faça login novamente.");
+}
+$id_perfil_logado = intval($_SESSION['id_perfil']);
+
+// Busca receitas favoritas do usuário logado
+$sql = "SELECT f.perfil_idperfil, f.receita_idreceita, 
+               r.nome_comida AS nome_receita, 
+               p.nome_perfil
+        FROM favoritos f
+        INNER JOIN receita r ON f.receita_idreceita = r.idreceita
+        INNER JOIN perfil p ON f.perfil_idperfil = p.idperfil
+        WHERE f.perfil_idperfil = $id_perfil_logado";
+
+$resultado = mysqli_query($conexao, $sql) or die("Erro ao buscar favoritos: " . mysqli_error($conexao));
 ?>
-<!DOCTYPE html>
-<html lang="pt-BR">
 
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Perfis</title>
-    <script src="../jquery-3.7.1.min.js"></script>
-    <link rel="stylesheet" href="style/style.css">
-    <script src="../jquery-3.7.1.min.js"></script>
-    <link rel="stylesheet" href="style/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Meus Favoritos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+<body>
+    <div class="container mt-5">
+        <h2>Receitas Favoritas</h2>
+        <table class="table table-striped mt-3">
+            <tr>
+                <th>Usuário</th>
+                <th>Receita</th>
+                <th>Ação</th>
+            </tr>
 
-<body class="bodyListar">
-    <div class="titulopesquisardiv">
-        <p class="titulopesquisar" id="listar">Brasil <br> Na Cozinha</p>
-    </div>
-
-    <div class="listartudo">
-
-
-        <?php
-        require_once "../conexao.php";
-        require_once "../funcoes.php";
-
-        $listar_fav = listarFavorito($conexao);
-
-        if (count($listar_fav) == 0) {
-            echo "Não existem perfis cadastrados";
-        } else {
-        ?>
-            <table border="1" class="table">
-                <tr>
-
-                    <td>Nome do perfil</td>
-                    <td>Nome da Receita</td>
-
-                </tr>
             <?php
-            foreach ($listar_fav as $fav) {
-                $perfil_idperfil = $fav['perfil_idperfil'];
-                $receita_idreceita = $fav['receita_idreceita'];
-                $nome_perfil = $fav['nome_perfil'];
-                $nome_receita = $fav['nome_receita'];
+            if (mysqli_num_rows($resultado) > 0) {
+                while ($linha = mysqli_fetch_assoc($resultado)) {
+                    $perfil_idperfil = $linha['perfil_idperfil'];
+                    $receita_idreceita = $linha['receita_idreceita'];
+                    $nome_perfil = htmlspecialchars($linha['nome_perfil']);
+                    $nome_receita = htmlspecialchars($linha['nome_receita']);
 
-                // <td colspan="2">Ação</td>
-
-
-                echo "<tr>";
-
-
-                echo "<td>$nome_perfil</td>";
-                echo "<td>$nome_receita</td>";
-                // echo "<td><a href='deletarFav.php?perfil=$perfil_idperfil&receita=$receita_idreceita'>Excluir</a></td>";
-                // echo "<td><a href='formFavoritos.php?perfil=$perfil_idperfil&receita=$receita_idreceita'>Editar</a></td>";
-
-                echo "</tr>";
+                    echo "<tr>";
+                    echo "<td>$nome_perfil</td>";
+                    echo "<td>$nome_receita</td>";
+                    echo "<td><a href='deletarFav.php?perfil=$perfil_idperfil&receita=$receita_idreceita' class='btn btn-danger btn-sm'>Excluir</a></td>";
+                    // echo "<td><a href='formFavoritos.php?perfil=$perfil_idperfil&receita=$receita_idreceita' class='btn btn-warning btn-sm'>Editar</a></td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='4'>Nenhuma receita favorita encontrada.</td></tr>";
             }
-        }
             ?>
-            </table>
+        </table>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
-
 </html>
